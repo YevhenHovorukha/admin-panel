3f1c651f1d6da6ffe30aaac4a3d4807d8f286f39
Yevhen Hovorukha <y.hovorukha@gmail.com>
2023-09-06 00:11:32 +0300

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  console.log(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route element={<h1>Protect</h1>}> */}
+        <Route path="/" element={<h1>Head</h1>}>
+          <Route index element={<h1>1</h1>} />
+          <Route path="first" element={<h1>2</h1>} />
+          <Route path="second" element={<h1>3</h1>} />
+        </Route>
+        {/* </Route> */}
+        <Route path="login" element={<h1>Login</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export interface IAmin {
+  i: number;
+}
